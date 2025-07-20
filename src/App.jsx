c076dc36a93d8d884505b4c2a891f8f7daad0395
muhammadfikri4 <muhfikriantoaji@gmail.com>
2025-07-20 22:41:32 +0700

    initial template himti revamp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Input } from "./features/global/components/Input";
+
+function App() {
+  return (
+    <>
+      <p>Hello World!</p>
+      <Input />
+    </>
+  );
+}
+
+export default App;
