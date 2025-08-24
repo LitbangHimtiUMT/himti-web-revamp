e212bcd32c0e6c2994694394f5fd7e1b2620f45a
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-08-24 20:49:31 +0700

    add BannerSection

--- src/features/home/view/HomePage.jsx
+++ src/features/home/view/HomePage.jsx
@@ -3,11 +3,13 @@ import MiniAboutSection from "../components/mini-about-section/MiniAboutSection"
 import PrestasiSection from "../components/prestasi-section/PrestasiSection";
 import ProgramSection from "../components/program-section/ProgramSection";
 import AlumniSection from "../components/alumni-section/AlumniSection";
+import BannerSection from "../components/hero-section/BannerSection";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <BannerSection />
       <MiniAboutSection />
       <PrestasiSection />
       <ProgramSection />
